0ec66268de6cd3451f89a997ec48927d40635469
Karthika D <karthikacseksrct@gmail.com>
2020-02-14 19:12:32 +0530

    CRUD commit

--- OnlineRealEstate/Controllers/LandController.cs
+++ OnlineRealEstate/Controllers/LandController.cs
@@ -10,6 +10,10 @@ namespace OnlineRealEstate.Controllers
 {
     public class LandController : Controller
     {
+        public ActionResult Index(Land land)
+        {
+            return RedirectToAction("DisplayLandDetails");
+        }
         // GET: Land
         public ActionResult DisplayLandDetails()
         {
@@ -23,5 +27,36 @@ namespace OnlineRealEstate.Controllers
         {
             return View();
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Land land)
+        {
+            LandRepositary.Add(land);
+            TempData["Message"] = "Added";
+            return RedirectToAction("DisplayLandDetails");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Land land = LandRepositary.GetLandById(id);
+            return View(land);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            LandRepositary.Delete(id);
+            return RedirectToAction("DisplayLandDetails");
+        }
+        [HttpPost]
+        public ActionResult Update(Land land)
+        {
+            LandRepositary.Update(land);
+            return RedirectToAction("DisplayLandDetails");
+
+        }
+         
     }
 }
--- OnlineRealEstateRepositary/LandRepositary.cs
+++ OnlineRealEstateRepositary/LandRepositary.cs
@@ -8,16 +8,38 @@ namespace OnlineRealEstateRepositary
 {
     public class LandRepositary
     {
-        public static List<Land> land=new List<Land>();
+        public static List<Land> lands=new List<Land>();
         static LandRepositary()
         {
-            land.Add(new Land { landId = 1, area = 3000, propertyType = "Flat", location = "Chennai" });
-            land.Add(new Land { landId = 2, area = 4000, propertyType = "Vila", location = "Bangalore" });
-            land.Add(new Land { landId = 3, area = 5000, propertyType = "Office Space" , location = "Erode" });
+            lands.Add(new Land { landId = 1, area = 3000, propertyType = "Flat", location = "Chennai" });
+            lands.Add(new Land { landId = 2, area = 4000, propertyType = "Vila", location = "Bangalore" });
+            lands.Add(new Land { landId = 3, area = 5000, propertyType = "Office Space" , location = "Erode" });
         }
         public static IEnumerable<Land> GetLandDetails()
         {
-            return land;
+            return lands;
+        }
+        public static void Add(Land land)
+        {
+            lands.Add(land);
+        }
+        public static void Delete(int landId)
+        {
+            Land land = GetLandById(landId);
+            if (land != null)
+                lands.Remove(land);
+        }
+        public static void Update(Land land)
+        {
+            Land landValue= lands.Find(id => id.landId == land.landId);
+            landValue.landId = land.landId;
+            landValue.area = land.area;
+            landValue.propertyType = land.propertyType;
+            landValue.location = land.location;
+        }
+        public static Land GetLandById(int landId)
+        {
+            return lands.Find(id => id.landId == landId);
         }
     }
 }
